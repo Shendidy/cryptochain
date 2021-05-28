5635dc13002ed19001cae01b7f5c885273997fca
Sherif Shendidy <sherif@shendidy.com>
2021-05-28 16:05:39 +0100

    created the hashing functionality

---
+++ crypto-hash.test.js
@@ -0,0 +1,13 @@
+const cryptoHash = require('./crypto-hash');
+
+describe('cryptoHash()', () => {
+  it('generates a SHA-256 hashed output', () => {
+    expect(cryptoHash('Test SHA256'))
+    .toEqual('48d738cce96b59f506c1bfc6bbf04fb70cd04e245f1bcf243c8487cc335b5505')
+  });
+
+  it('produces the same hash with the same input arguments in any order', () => {
+    expect(cryptoHash('one', 'two', 'three'))
+    .toEqual(cryptoHash('three', 'one', 'two'));
+  });
+});
