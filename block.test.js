3d54b2ec1973c32573514d4b1b808346160db3f7
Sherif Shendidy <sherif@shendidy.com>
2021-05-28 16:15:51 +0100

    complete the block class

--- block.test.js
+++ block.test.js
@@ -1,5 +1,6 @@
 const Block = require('./block');
 const { GENESIS_DATA } = require('./config');
+const cryptoHash = require('./crypto-hash');
 
 describe('Block', () => {
   const timestamp = 'A time stamp';
@@ -29,6 +30,33 @@ describe('Block', () => {
 
     it('returns the genesis data', () => {
       expect(genesisBlock).toEqual(GENESIS_DATA);
+    });
+  });
+
+  describe('mineBlock()', () => {
+    const lastBlock = Block.genesis();
+    const data = 'new data';
+    const minedBlock = Block.mineBlock({lastBlock, data});
+
+    it('returns a Block instance', () => {
+      expect(minedBlock instanceof Block).toBe(true);
+    });
+
+    it('sets the `lastHash` to be the `hash` of the lastBlock', () => {
+      expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+    });
+
+    it('sets the `data`', () => {
+      expect(minedBlock.data).toEqual(data);
+    });
+
+    it('sets a `timestamp`', () => {
+      expect(minedBlock.timestamp).not.toEqual(undefined);
+    });
+
+    it('creates a SHA256 `hash` based on the properinputs', () => {
+      expect(minedBlock.hash)
+      .toEqual(cryptoHash(minedBlock.timestamp, data, lastBlock.hash))
     });
   });
 });
