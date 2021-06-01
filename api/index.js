3a35cf7d9c2cbf724114bf7127d505020598235d
sherif@shendidy.com <sherif@shendidy.com>
2021-06-01 18:21:15 +0100

    added the post request

--- api/index.js
+++ api/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../src/blockchain');
 
 const app = express();
 const blockchain = new Blockchain();
 
-for(let i = 0; i<50; i++)
+app.use(bodyParser.json());
+
+for(let i = 0; i<5; i++)
   blockchain.addBlock({data: `new data: ${i}`});
 
 app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req, res) => {
+  const { data } = req.body;
+
+  blockchain.addBlock({ data});
+
+  res.redirect('/api/blocks');
 });
 
 const PORT = 3000;
